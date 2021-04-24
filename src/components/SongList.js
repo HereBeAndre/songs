18702a38e4a7f701fb30ace01d86362aa7d0d88a
atotared <atota@red.software.systems>
2021-04-24 17:39:03 +0200

    Update selectedSong on button click

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,8 +1,36 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { connect, useDispatch } from 'react-redux';
+import { setSelectSong } from '../actions';
 
-const SongList = () => {
-  return <div>SongList</div>;
+const SongList = ({ songList, selectedSong }) => {
+  const dispatch = useDispatch();
+  const renderList = songList.map((song) => {
+    return (
+      <div className="item" key={song.title}>
+        <div className="right floated content">
+          <button
+            className="ui button primary"
+            onClick={() => {
+              selectedSong(song);
+            }}
+          >
+            Select
+          </button>
+        </div>
+        <div className="content">
+          <h3>{song.title}</h3>
+          <h5>{song.artist}</h5>
+        </div>
+      </div>
+    );
+  });
+  return <div className="ui divided list">{renderList}</div>;
 };
 
-export default connect()(SongList);
+const mapStateToProps = (state) => {
+  return { songList: state.songList };
+};
+
+export default connect(mapStateToProps, {
+  selectedSong: setSelectSong,
+})(SongList);
